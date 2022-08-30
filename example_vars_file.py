19b59125a25531413969eae204caab7fdd54cb6d
ebronson68 <111298136+ebronson68@users.noreply.github.com>
2022-08-30 13:59:33 -0500

    added example vars file

---
+++ example_vars_file.py
@@ -0,0 +1,6 @@
+# replace variables as needed
+imgur_client_id = CLIENT_ID
+imgur_client_secret = CLIENT_SECRET
+reddit_client_id = CLIENT_ID
+reddit_client_secret = CLIENT_SECRET
+reddit_user_agent = USER_AGENT
